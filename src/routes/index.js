a3c15061512a974a8e0ea72ba5cff2e4be2fe80a
Maxime Hébert <83720884+MaximeMartz@users.noreply.github.com>
2024-10-04 10:19:41 +0200

    feat: add crud operations for genres

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 const artists = require("./artists.routes");
 const venues = require("./venues.routes");
+const genres = require("./genres.routes");
 const router = require("express").Router();
 
 router.use("/api/artists", artists);
 router.use("/api/venues", venues);
+router.use("/api/genres", genres);
 
 module.exports = router;
