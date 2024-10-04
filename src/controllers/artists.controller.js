3f86fbafdbafe2fd56c409786a05c56f4a079ab7
Maxime Hébert <83720884+MaximeMartz@users.noreply.github.com>
2024-10-04 09:55:15 +0200

    feat: add crud operations for artists

--- src/controllers/artists.controller.js
+++ src/controllers/artists.controller.js
@@ -1,12 +1,56 @@
 const {
     createArtist,
+    getArtists,
+    getArtistById,
+    updateArtist,
+    deleteArtist
 } = require("../queries/artists.queries");
 
 exports.addArtist = async (req, res) => {
     try {
         const body = req.body;
-            await createArtist(body);
-            res.status(201).json({ message: "Artist created successfully" });
+        await createArtist(body);
+        res.status(201).json({ message: "Artist created successfully" });
+    } catch (e) {
+        throw e;
+    }
+};
+
+exports.artistsList = async (req, res) => {
+    try {
+        const artists = await getArtists();
+        res.json({ artists });
+    } catch (e) {
+        throw e;
+    }
+};
+
+exports.artistDetails = async (req, res) => {
+    try {
+        const artistId = req.params.artistId;
+        const artist = await getArtistById(artistId);
+        res.json({ artist });
+    } catch (e) {
+        throw e;
+    }
+};
+
+exports.editArtist = async (req, res) => {
+    const artistId = req.params.artistId;
+    try {
+        const body = req.body;
+        await updateArtist(artistId, body);
+        res.status(201).json({ message: "Artist updated successfully" });
+    } catch (e) {
+        throw e;
+    }
+};
+
+exports.removeArtist = async (req, res) => {
+    const artistId = req.params.artistId;
+    try {
+        await deleteArtist(artistId);
+        res.status(201).json({ message: "Artist deleted successfully" });
     } catch (e) {
         throw e;
     }
--- src/queries/artists.queries.js
+++ src/queries/artists.queries.js
@@ -7,11 +7,54 @@ exports.createArtist = async (body) => {
             bio: body.bio,
             genres: body.genres,
             image: body.image,
-            socialMedia: body.socialMedia,
+            socialMedia: {
+                instagram: body.instagram,
+                twitter: body.twitter,
+            },
             spotify: body.spotify,
         });
         return artist.save();
     } catch (e) {
         throw e;
     }
+};
+
+exports.getArtists = async () => {
+    try {
+        return Artist.find({});
+    } catch (e) {
+        throw e;
+    }
+};
+
+exports.getArtistById = async (id) => {
+    try {
+        return Artist.findOne({_id: id}).select("-__v");
+    } catch (e) {
+        throw e;
+    }
+};
+
+exports.updateArtist = async (artistId, body) => {
+    return Artist.findByIdAndUpdate(
+        artistId,
+        {
+            $set: {
+                name: body.name,
+                bio: body.bio,
+                genres: body.genres,
+                image: body.image,
+                socialMedia: {
+                    instagram: body.instagram,
+                    twitter: body.twitter,
+                },
+                spotify: body.spotify,
+            },
+        },
+        {runValidators: true}
+    );
+};
+
+exports.deleteArtist = async (artistId) => {
+    return Artist.findByIdAndDelete(artistId);
 };
